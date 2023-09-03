f4f059cb7ba1cdf0b09065de17f22c0e5ce58b9e
Aziz <aziznal.dev@gmail.com>
2023-09-03 20:07:39 +0300

    loops are cool

--- hello-world.go
+++ hello-world.go
@@ -8,5 +8,19 @@ func main() {
     k := "Hello World, Again!";
 
     fmt.Println(k);
+
+    loop();
+}
+
+func loop() {
+    for i := 0; i <= 10; i++ {
+        fmt.Print(fmt.Sprint(i) + ", ");
+    }
+
+    var foo float32 = 0
+
+    for ; foo < 10; {
+        foo += 0.1
+    }
 }
 
