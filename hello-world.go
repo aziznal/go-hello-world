a3556206b06ac1b93b0437b6d8ba549ed6dee82f
Aziz <aziznal.dev@gmail.com>
2023-09-03 20:27:04 +0300

    try switch case

--- hello-world.go
+++ hello-world.go
@@ -1,26 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 func main() {
-    fmt.Println("Hello World!")
+	// fmt.Println("Hello World!")
+	//
+	// k := "Hello World, Again!";
+	//
+	// fmt.Println(k);
 
-    k := "Hello World, Again!";
+	// loop();
 
-    fmt.Println(k);
-
-    loop();
+	switchCase()
 }
 
 func loop() {
-    for i := 0; i <= 10; i++ {
-        fmt.Print(fmt.Sprint(i) + ", ");
-    }
+	for i := 0; i <= 10; i++ {
+		fmt.Print(fmt.Sprint(i) + ", ")
+	}
 
-    var foo float32 = 0
+	var foo float32 = 0
 
-    for ; foo < 10; {
-        foo += 0.1
-    }
+	for foo < 10 {
+		foo += 0.1
+	}
 }
 
+func switchCase() {
+	os := runtime.GOOS
+
+	switch os {
+	case "linux":
+		fmt.Println("You're awesome")
+	default:
+		fmt.Println(os)
+	}
+}
